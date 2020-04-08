7a1642b21dece444f39f4b5ce755fc570d056b11
yuta <yuta.fukui@seattleconsulting.co.jp>
2020-04-08 18:28:19 +0900

    add_file

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="headerSection">
+    <button @click="toggle" class="btn btn-success"><img src="sozai_cman_jp_20200408124046.png"></button>
+    <Drawer @close="toggle" align="left" :closeable="true">
+      <div v-if="open"></div>
+    </Drawer>
+  </div>
+</template>
+ 
+<script>
+import Drawer from "vue-simple-drawer";
+ 
+export default {
+  name: "App",
+  components: {
+    Drawer
+  },
+  data() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.open = !this.open
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap-reboot",
+"~bootstrap/scss/buttons";
+</style>
+<style lang="scss" scoped>
+    #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #f3f3f3;
+  margin: 0 auto;
+  width: 800px;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="drawerSection">
+        <img src="" class="drawerButton">
+        <ul class="drawerMenuSection">
+            <li class="drawerMenuItem">
+                <a herf="" class="drawerMenuItemLink">Home</a>
+            </li>
+            <li class="drawerMenuItem">
+                <a herf="" class="drawerMenuItemLink">About Me</a>
+            </li>
+            <li class="drawerMenuItem">
+                <a herf="" class="drawerMenuItemLink">Skill Sets</a>
+            </li>
+            <li class="drawerMenuItem">
+                <a herf="" class="drawerMenuItemLink">Vision</a>
+            </li>
+            <li class="drawerMenuItem">
+                <a herf="" class="drawerMenuItemLink">Custmoize Item1</a>
+            </li>
+            <li class="drawerMenuItem">
+                <a herf="" class="drawerMenuItemLink">Custmoize Item2</a>
+            </li>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    #drawerSection {
+        background-color: #f3f3f3;
+        width: 100%;
+        height: auto;
+    }
+
+    .drawerMenuSection  .drawerMenuItem {
+        background-color: #ffffff;
+        width: 100%;
+        height: auto;
+    }
+</style>
